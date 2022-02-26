e7509122118bdc4da6573d421651707435a096b9
Abhijeet Prasad <abhi.prasad16@gmail.com>
2022-02-26 23:25:01 +0530

    init project

---
+++ app/src/main/java/com/upes/employeeonboarding/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.upes.employeeonboarding
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
